212e87a0b42e32e15dd6eafbe8a1354005cb2a76
Kamali20072002 <kamali@outdidtech.com>
2025-02-17 14:27:25 +0530

    init

---
+++ lib/feature/more/presentation/pages/banner_image/banner_image.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BannerImage extends StatelessWidget {
+  const BannerImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
